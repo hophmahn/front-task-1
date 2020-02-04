d74dd56ccdd6659460da2094f3f35c336b42bcd0
Dastan Ahmet <dastanahmet01@gmail.com>
2020-02-05 03:10:59 +0600

    Add function to prepare costs and parents

--- src/solution.js
+++ src/solution.js
@@ -1,3 +1,19 @@
-const solution = function(graph, start, finish)  {
-    // Ваше решение
+const prepareCostsAndParents = function(graph, start, finish) {
+    const costs = { [finish]: Infinity };
+    const parents = { [finish]: null };
+
+    for (let key in graph[start]) {
+        const cost = graph[start][key];
+        costs[key] = cost;
+        parents[key] = start;
+    }
+
+    return {
+        costs,
+        parents
+    }
+}
+
+const solution = function(graph, start, finish) {
+    const { costs, parents } = prepareCostsAndParents(graph, start, finish);
 }
